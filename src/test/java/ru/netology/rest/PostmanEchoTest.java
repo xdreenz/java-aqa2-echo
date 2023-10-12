efb078c53e3cd045e361b4b7f56768a4384552de
Akaten test <gxconv@gmail.com>
2023-10-12 23:19:54 +0300

    And yet one failing build trigger

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
         .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("somedata"))
         ;
     }
 }
