1f070aeb6b032cbb33893671677f653d1ed46651
Akaten test <gxconv@gmail.com>
2023-10-12 23:06:40 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnPOSTData() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+        .when()
+                .post("/post")
+// Проверки
+        .then()
+                .contentType(JSON)
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
